d380d0b0d8550f6f8dc3a2467b8b9ac643380a53
mbuijames <35819739+mbuijames@users.noreply.github.com>
2025-10-24 19:27:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,8 +34,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// 1) API routes first
-app.use("/api", apiRouter);
+
 
 // 3) SPA fallback (for anything NOT starting with /api)
 app.get("*", (req, res) => {
