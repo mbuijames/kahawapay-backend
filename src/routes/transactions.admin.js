f402b29b0c1e3bc5d479c501e76cd8bd7eaaa9c4
mbuijames <35819739+mbuijames@users.noreply.github.com>
2026-01-16 19:11:14 +0300

    Update transactions.admin.js

--- src/routes/transactions.admin.js
+++ src/routes/transactions.admin.js
@@ -2,6 +2,7 @@
 import express from "express";
 import sequelize from "../db.js";
 import { QueryTypes } from "sequelize";
+import nodemailer from "nodemailer";
 
 const router = express.Router();
 
